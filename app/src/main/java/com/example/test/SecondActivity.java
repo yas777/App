8df349cc6f97a2edf91bd751c2ad73753d8570bb
Yaswanth <yaswanthorru123@gmail.com>
2018-07-13 17:20:54 +0530

    image upload added to form_layout page

--- app/src/main/java/com/example/test/SecondActivity.java
+++ app/src/main/java/com/example/test/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.example.test;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,16 +18,26 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class SecondActivity extends Activity implements AdapterView.OnItemSelectedListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_layout);
+
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1001);
+        }
 
         EditText mdate = findViewById(R.id.edit_date);
         Date dNow = new Date();
@@ -34,6 +48,22 @@ public class SecondActivity extends Activity implements AdapterView.OnItemSelect
         String time = ft.format(dNow);
         mdate.setText(date);
         mdate.setEnabled(false);
+
+        Button but = (Button)findViewById(R.id.button);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialFilePicker()
+                        .withActivity(SecondActivity.this)
+                        .withRequestCode(1000)
+                        .withHiddenFiles(true) // Show hidden files and folders
+                        .start();
+
+
+            }
+        });
+
+
         // Spinner element
         final Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
@@ -77,4 +107,28 @@ public class SecondActivity extends Activity implements AdapterView.OnItemSelect
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            // Do anything with file
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1001: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,"Permission granted",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this,"Permission Denied",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
 }
